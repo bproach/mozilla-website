3268f50edfa06f5528cfd42ccb41e015fb13e8d0
Brendan Roach <brendan.roach.6@gmail.com>
2017-01-12 14:59:50 -0500

    add image change on click and set uniform image dimensions

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-document.querySelector('img').onclick = function(){
-	alert("Ow, stop poking me!");
+var myPicture = document.querySelector('img');
+myPicture.onclick = function(){
+	var mySrc = myPicture.getAttribute('src');
+	if(mySrc === 'http://mdn.github.io/beginner-html-site/images/firefox-icon.png'){
+		myPicture.setAttribute('src', 'http://icons.iconarchive.com/icons/media-design/hydropro/512/HP-Firefox-icon.png');
+	}
+	else {
+		myPicture.setAttribute('src', 'http://mdn.github.io/beginner-html-site/images/firefox-icon.png');
+	}
+
 }
